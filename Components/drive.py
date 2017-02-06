05b06dd2c62256cde1d46c00fe7084ad8b6fabe4
Dieter Brehm <inkering@gmail.com>
2017-02-05 23:08:21 -0500

    Converted to magic robot for increased modularity. Did some object prototyping for what we will need this year.

---
+++ Components/drive.py
@@ -0,0 +1,7 @@
+import wpilib
+
+from robotpy_ext.common_drivers import navx, distance_sensors
+from networktables import NetworkTable
+from networktables.util import ntproperty
+import math
+
--- robot.py
+++ robot.py
@@ -1,10 +1,44 @@
-from enum import Enum
+import magicbot
 import wpilib
-import math
 
-class MyRobot(wpilib.IterativeRobot):
-    def robotInit(self):
-    def autonomousInit(self):
-    def autonomousPeriodic(self):
-    def teleopInit(self):
-    def teleopPeriodic(self):
+import pytest
+
+from robotpy_ext.control.button_debouncer import ButtonDebouncer
+from robotpy_ext.common_drivers import navx, distance_sensors
+
+from networktables.networktable import NetworkTable
+
+class myRobot(magicbot.MagicRobot):
+    def createObjects(self):
+        #Sticks
+        self.driverStick = wpilib.Joystick(1)
+        self.operatorStick = wpilib.Joystick(0)
+
+        #CAN Talons
+        self.lf = wpilib.CANTalon(2)
+        self.lr = wpilib.CANTalon(3)
+        self.rf = wpilib.CANTalon(4)
+        self.rb = wpilib.CANTalon(5)
+
+        #robot drive to eventually control our mecanum drive train
+        self.robotDrive = wpilib.RobotDrive(self.lf, self.lr, self.rf, self.rb)
+
+        #Intake motor
+        self.intakeController = wpilib.CANTalon(6)
+
+        #Shooter motor
+        self.intakeController = wpilib.CANTalon(7)
+
+        #Climber motor
+        self.climbController = wpilib.CANTalon(8)
+
+        #agitator
+        # Maybe be deprecated because of a simpler solution
+        self.aggitController = wpilib.CANTalon(9)
+
+        # IMU Gyroscope
+        self.navX = navx.AHRS.create_spi()
+
+        #network tables
+        self.sd = NetworkTable.getTable('smartdashboard')
+        self.viz = NetworkTable.getTable('visiondashboard')
